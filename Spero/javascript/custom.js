a5f4f5c2a9a1fd26c0b27feebe6b551f995ba08e
poojasingh0830 <124341645+poojasingh0830@users.noreply.github.com>
2023-05-31 23:09:08 +0530

    Add files via upload

---
+++ Spero/javascript/custom.js
@@ -0,0 +1,119 @@
+// var a = 2;
+// var b = "Admin"
+// var admin = 12
+
+// 5 > 5  false
+// 5 < 6  true
+// 5 > 4  true
+
+// function buttonOnClick() {
+//    let user = "user 1";
+//    document.getElementById("message").innerHTML = user;
+// }
+
+// let navigation = ["Home", "About", "Product", "Services", "Contact"];
+
+// for (let i = 0; i < navigation.length; i++) {
+//   console.log(navigation[i]);
+// }
+
+// Loop  for, do while, while
+
+// let counter = 12
+
+// while(counter <= 10){
+//     document.write(" counter")
+//     counter = counter + 1;
+// }
+
+// do{
+//     document.write(" Amit")
+//     counter = counter + 1;
+// }while(counter <= 10)
+
+// for(let counter = 1; counter <= 10; counter++){
+//     document.write(" counter")
+// }
+
+// let x = 10;
+// x = x + 2
+// x += 2
+
+// ex: 1
+
+// function handleButtonClick() {
+//   document.getElementById("message").innerHTML = "Hello World";
+// }
+
+// ex: 2
+
+// document.getElementById("message").addEventListener("click", handleClickWithAddEventLestener);
+// function handleClickWithAddEventLestener() {
+//   document.getElementById("message").innerHTML = "YOU CLICKED ME!";
+// }
+
+// function scrollToTop() {
+//   window.scrollTo({ top: 0, behavior: "smooth" });
+// }
+
+// const links = document.querySelectorAll(".page-header ul a");
+
+// for (const link of links) {
+//   link.addEventListener("click", clickHandler);
+// }
+
+// function clickHandler(e) {
+//   e.preventDefault();
+
+//   const href = this.getAttribute("href");
+//   const offsetTop = document.querySelector(href).offsetTop;
+
+//   scroll({
+//     top: offsetTop,
+//     behavior: "smooth"
+//   });
+// }
+
+// let subject = ["Maths","Physics","Bio","Computer science"];
+
+// let arrayToText = subject.join("-")
+
+// let subject2 = ["Hindi","English"];
+
+// let newArray = subject.concat(subject2)
+
+// console.log(newArray);
+
+// $(".default_option").click(function () {
+//   $(this).parent().toggleClass("active");
+// });
+
+// $(".select_ul li").click(function () {
+//   var currentele = $(this).html();
+//   $(".default_option li").html(currentele);
+//   $(this).parents(".select_wrap").removeClass("active");
+// });
+
+// $(".buttonclick").click(function(event){
+//   event.stopPropagation();
+//   alert("button click")
+// })
+
+// $(".box").click(function(){
+//   alert("box click")
+// })
+
+
+// $(".checkbox-input").click(function(event){
+//   event.preventDefault();
+// })
+
+//   $("#btn1").click(function(){
+//    testText =  $("#test").text();
+//    $("#newText").replace('amit','pushpandra')
+//   });
+  
+//   $("#btn2").click(function(){
+//     alert($("#test").html());
+//   });
+
